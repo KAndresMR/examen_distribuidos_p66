6b93ceea0258afd3ecb438f650977af31f803d12
KAndresMR <109705281+KAndresMR@users.noreply.github.com>
2025-05-28 18:37:37 -0500

    final

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit{
+  title = 'WebSocket';
+  messages: string[] = [];
+
+  ngOnInit() {
+    const socket = new WebSocket('ws://localhost:3000');
+    socket.onmessage = (event) => {
+      this.messages.push(event.data);
+    };
+  }
 }
